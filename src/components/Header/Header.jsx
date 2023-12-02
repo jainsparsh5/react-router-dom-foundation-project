47b695de0f83cca80e1d9621ab8a4f005107b21d
SparshJain <jainsparsh17@gmail.com>
2023-12-02 14:36:49 +0530

    framer motion added

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,53 +1,105 @@
-import React from 'react'
-import { Link, NavLink } from 'react-router-dom'
+import React from "react";
+import { Link, NavLink } from "react-router-dom";
+import { motion } from "framer-motion";
 
 export default function Header() {
-    return (
-        <header className="shadow sticky z-50 top-0">
-            <nav className="bg-white border-gray-200 px-4 lg:px-6 py-2.5">
-                <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
-                    <Link to="/" className="flex items-center">
-                        <img
-                            src="https://alexharkness.com/wp-content/uploads/2020/06/logo-2.png"
-                            className="mr-3 h-12"
-                            alt="Logo"
-                        />
-                    </Link>
-                    <div className="flex items-center lg:order-2">
-                        <Link
-                            to="#"
-                            className="text-gray-800 hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 focus:outline-none"
-                        >
-                            Log in
-                        </Link>
-                        <Link
-                            to="#"
-                            className="text-white bg-orange-700 hover:bg-orange-800 focus:ring-4 focus:ring-orange-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 focus:outline-none"
-                        >
-                            Get started
-                        </Link>
-                    </div>
-                    <div
-                        className="hidden justify-between items-center w-full lg:flex lg:w-auto lg:order-1"
-                        id="mobile-menu-2"
-                    >
-                        <ul className="flex flex-col mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0">
-                            <li>
-                                <NavLink
-                                    className={() =>
-                                        `block py-2 pr-4 pl-3 duration-200 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-orange-700 lg:p-0`
-                                    }
-                                >
-                                    Home
-                                </NavLink>
-                            </li>
-                            
-                            
-                        </ul>
-                    </div>
-                </div>
-            </nav>
-        </header>
-    );
+  const buttonVariants = {
+    hover: {
+      opacity: 0.75,
+      transition: {
+        duration: 0.3,
+      },
+    },
+  };
+  return (
+    <header className="shadow sticky z-50 top-0">
+      <nav className="bg-white border-gray-200 px-4 lg:px-6 py-2.5">
+        <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
+          <motion.div
+            variants={buttonVariants}
+            whileHover={{ scale: 1.1 }}
+            whileTap={{ scale: 0.9 }}
+            transition={{ type: "spring", stiffness: 400, damping: 17 }}
+          >
+            <Link to="/" className="flex items-center">
+              <img
+                src="https://alexharkness.com/wp-content/uploads/2020/06/logo-2.png"
+                className="mr-3 h-12"
+                alt="Logo"
+              />
+            </Link>
+          </motion.div>
+          <div className="flex items-center lg:order-2">
+            <motion.div
+              variants={buttonVariants}
+              whileHover={{ scale: 1.1 }}
+              whileTap={{ scale: 0.9 }}
+              transition={{ type: "spring", stiffness: 400, damping: 17 }}
+            >
+              <Link
+                to="#"
+                className="text-gray-800 hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 focus:outline-none"
+              >
+                Log in
+              </Link>
+            </motion.div>
+            <motion.div
+              variants={buttonVariants}
+              whileHover={{ scale: 1.1 }}
+              whileTap={{ scale: 0.9 }}
+              transition={{ type: "spring", stiffness: 400, damping: 17 }}
+            >
+              <Link
+                to="#"
+                className="text-white bg-orange-700 hover:bg-orange-800 focus:ring-4 focus:ring-orange-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 focus:outline-none"
+              >
+                Get started
+              </Link>
+            </motion.div>
+          </div>
+          <div
+            className="hidden justify-between items-center w-full lg:flex lg:w-auto lg:order-1"
+            id="mobile-menu-2"
+          >
+            <ul className="flex flex-col mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0">
+              <li>
+                <motion.div
+                  whileTap={{ scale: 0.9 }}
+                  transition={{ type: "spring", stiffness: 400, damping: 17 }}
+                >
+                  <NavLink
+                    to="/"
+                    className={({ isActive }) =>
+                      `block py-2 pr-4 pl-3 duration-200 ${
+                        isActive ? "text-orange-700" : "text-gray-700"
+                      } border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-orange-700 lg:p-0`
+                    }
+                  >
+                    Home
+                  </NavLink>
+                </motion.div>
+              </li>
+              <li>
+                <motion.div
+                  whileTap={{ scale: 0.9 }}
+                  transition={{ type: "spring", stiffness: 400, damping: 17 }}
+                >
+                  <NavLink
+                    to="/about"
+                    className={({ isActive }) =>
+                      `block py-2 pr-4 pl-3 duration-200 ${
+                        isActive ? "text-orange-700" : "text-gray-700"
+                      } border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-orange-700 lg:p-0`
+                    }
+                  >
+                    About
+                  </NavLink>
+                </motion.div>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </header>
+  );
 }
-
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,38 +1,110 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { Link } from "react-router-dom";
+import { motion, useAnimation } from "framer-motion";
 
 export default function Home() {
+
+  const buttonVariants = {
+    hover: {
+      opacity: 0.75,
+      transition: {
+        duration: 0.3,
+      },
+    },
+  };
+
+  const imageVariants = {
+    hidden: { opacity: 0, y: 50 },
+    visible: {
+      opacity: 1,
+      y: 0,
+      transition: {
+        type: "spring",
+        damping: 10,
+        stiffness: 100,
+      },
+    },
+  };
+
+  const popupVariants = {
+    hidden: { opacity: 0, scale: 0.8 },
+    visible: {
+      opacity: 1,
+      scale: 1,
+      transition: {
+        duration: 0.7,
+      },
+    },
+  };
+
+  const popupControls = useAnimation();
+
+  useEffect(() => {
+    // Trigger the animation when the component mounts
+    popupControls.start("visible");
+  }, [popupControls]);
+
+  const controls = useAnimation();
+
+  const handleScroll = () => {
+    const scrollY = window.scrollY;
+
+    if (scrollY > 100) {
+      controls.start("visible");
+    } else {
+      controls.start("hidden");
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   return (
     <div className="mx-auto w-full max-w-7xl">
       <aside className="relative overflow-hidden text-black rounded-lg sm:mx-16 mx-2 sm:py-16">
         <div className="relative z-10 max-w-screen-xl px-4  pb-20 pt-10 sm:py-24 mx-auto sm:px-6 lg:px-8">
           <div className="max-w-xl sm:mt-1 mt-80 space-y-8 text-center sm:text-right sm:ml-auto">
             <h2 className="text-4xl font-bold sm:text-5xl">
-              Download Now
-              <span className="hidden sm:block text-4xl">Lorem Ipsum</span>
+              Empower your business with{" "}
+              <span className="text-orange-700">Strategic</span> insights
             </h2>
+            <span className="hidden sm:block  mt-5 text-sm">Powerful management platform designed to streamline your business operations, boost productivity, and drive success</span>
 
-            <Link
-              className="inline-flex text-white items-center px-6 py-3 font-medium bg-orange-700 rounded-lg hover:opacity-75"
-              to="/"
+            <motion.div
+              variants={buttonVariants}
+              whileHover={{ scale: 1.1 }}
+              whileTap={{ scale: 0.9 }}
+              transition={{ type: "spring", stiffness: 400, damping: 17 }}
+              className="inline-flex text-white items-center justify-center px-6 py-3 font-medium bg-orange-700 rounded-lg"
             >
-              <svg
-                fill="white"
-                width="24"
-                height="24"
-                xmlns="http://www.w3.org/2000/svg"
-                fillRule="evenodd"
-                clipRule="evenodd"
-              >
-                <path d="M1.571 23.664l10.531-10.501 3.712 3.701-12.519 6.941c-.476.264-1.059.26-1.532-.011l-.192-.13zm9.469-11.56l-10.04 10.011v-20.022l10.04 10.011zm6.274-4.137l4.905 2.719c.482.268.781.77.781 1.314s-.299 1.046-.781 1.314l-5.039 2.793-4.015-4.003 4.149-4.137zm-15.854-7.534c.09-.087.191-.163.303-.227.473-.271 1.056-.275 1.532-.011l12.653 7.015-3.846 3.835-10.642-10.612z" />
-              </svg>
-              &nbsp; Download now
-            </Link>
+              <Link to="/" className="inline-flex">
+                <svg
+                  fill="white"
+                  width="24"
+                  height="24"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fillRule="evenodd"
+                  clipRule="evenodd"
+                >
+                  <path d="M1.571 23.664l10.531-10.501 3.712 3.701-12.519 6.941c-.476.264-1.059.26-1.532-.011l-.192-.13zm9.469-11.56l-10.04 10.011v-20.022l10.04 10.011zm6.274-4.137l4.905 2.719c.482.268.781.77.781 1.314s-.299 1.046-.781 1.314l-5.039 2.793-4.015-4.003 4.149-4.137zm-15.854-7.534c.09-.087.191-.163.303-.227.473-.271 1.056-.275 1.532-.011l12.653 7.015-3.846 3.835-10.642-10.612z" />
+                </svg>
+                &nbsp; Download now
+              </Link>
+            </motion.div>
           </div>
         </div>
 
-        <div className="absolute inset-0 w-full sm:my-20 sm:pt-1 pt-12 h-full ">
-          <img
+        <div className="absolute inset-0 w-full sm:my-10 sm:pt-1 pt-12 h-full">
+          <motion.img
             className="w-96"
+            variants={popupVariants}
+            initial="hidden"
+            animate={popupControls}
             src="https://i.ibb.co/5BCcDYB/Remote2.png"
             alt="image1"
           />
@@ -40,10 +112,13 @@ export default function Home() {
       </aside>
 
       <div className="grid  place-items-center sm:mt-20">
-        <img
-          className="sm:w-96 w-48"
+        <motion.img
+          className="w-96"
           src="https://i.ibb.co/2M7rtLk/Remote1.png"
           alt="image2"
+          variants={imageVariants}
+          initial="hidden"
+          animate={controls}
         />
       </div>
 
